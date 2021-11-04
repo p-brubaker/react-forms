1ecc85d48ce588f67260d601be68c5c7d4902cc8
Paul Brubaker <pbrubak@gmail.com>
2021-11-04 13:30:18 -0700

    passing behavioral test

--- src/containers/NewsSearchContainer.test.jsx
+++ src/containers/NewsSearchContainer.test.jsx
@@ -1,31 +1,65 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {
+    render,
+    screen,
+    waitFor,
+    waitForElementToBeRemoved,
+} from '@testing-library/react';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import NewsSearchContainer from './NewsSearchContainer';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import mockDefaultGetResults from './mockDefaultGetResults.json';
+import mockGetWithQuery from './mockGetWithQuery.json';
+const URL = 'https://newsapi.org/v2/everything';
 
 describe('Shows news articles', () => {
-    it('should display a list of news articles', async () => {
+    const server = setupServer(
+        rest.get(URL, (req, res, ctx) => {
+            const query = req.url.searchParams;
+            const q = query.get('q');
+            const domains = query.get('domains');
+
+            if (q === ' JavaScript' && domains === 'hackaday.com') {
+                return res(ctx.status(200), ctx.json(mockGetWithQuery));
+            } else {
+                return res(ctx.status(200), ctx.json(mockDefaultGetResults));
+            }
+        })
+    );
+
+    beforeAll(() => {
+        server.listen({ onUnhandledRequest: 'warn' });
+    });
+
+    afterEach(() => server.resetHandlers);
+
+    afterAll(() => {
+        server.close();
+    });
+
+    it('should display a list of articles mentioning JavaScript', async () => {
         render(<NewsSearchContainer />);
         screen.getByText('Loading...');
-
-        const ul = await screen.findByRole('list', { name: 'articles' });
-        expect(ul).toMatchSnapshot();
 
         const queryInput = await screen.findByLabelText('Search by topic');
         userEvent.type(queryInput, 'JavaScript');
 
         const submitButton = await screen.findByRole('button', {
-            name: 'Submit',
+            name: 'submit',
         });
 
         userEvent.click(submitButton);
+
+        await waitForElementToBeRemoved(screen.getByText('Loading...'));
 
         return waitFor(() => {
             const articles = screen.getAllByText('JavaScript', {
                 exact: false,
             });
 
-            expect(articles).toHaveLength(10);
+            expect(articles).toHaveLength(1);
         });
     });
 });
